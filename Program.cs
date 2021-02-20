914421082adc7cc886805bb273f383898f0e3532
Simplevolk <simplevolk@mail.ru>
2021-02-20 21:14:12 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MailRuCupMiner.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Core;
+
+namespace MailRuCupMiner
+{
+    class Program
+    {
+        public static Logger Logger;
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Logger = CreateLogger();
+
+            using IHost host = CreateHostBuilder(args).Build();
+            host.RunAsync();
+            host.Services.GetService<IMainWorker>()?.Run();
+            Console.ReadLine();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).ConfigureServices((_, services) =>
+            {
+                services.AddHttpClient();
+                services.AddSingleton<IMainWorker, MainWorker>();
+            });
+        }
+
+        static Logger CreateLogger()
+        {
+            using var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            return log;
+        }
+    }
+}
---
+++ Services/MainWorker.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Mainerspace;
+
+namespace MailRuCupMiner.Services
+{
+    public interface IMainWorker
+    {
+        void Run();
+        void Stop();
+        void Dispose();
+    }
+
+    public class MainWorker : IMainWorker
+    {
+        private Client _client;
+        private IHttpClientFactory _clientFactory;
+        public MainWorker(IHttpClientFactory httpClientFactory)
+        {
+            _clientFactory = httpClientFactory;
+            _client=new Client("default",_clientFactory.CreateClient());
+        }
+        public void Run()
+        {
+            while (true)
+            {
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
