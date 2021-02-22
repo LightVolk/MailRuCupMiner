024d6c37e08796419ff35c270e6f7b65876ee1f8
Simplevolk <simplevolk@mail.ru>
2021-02-22 22:10:33 +0300

    add license service mock tests

---
+++ Clients/IClient.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mainerspace;
+
+namespace MailRuCupMiner.Clients
+{
+    public interface IClient
+    {
+        string BaseUrl { get; set; }
+        bool ReadResponseAsString { get; set; }
+
+        /// <returns>Extra details about service status, if any.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<object> HealthCheckAsync();
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Extra details about service status, if any.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<object> HealthCheckAsync(System.Threading.CancellationToken cancellationToken);
+
+        /// <returns>Current balance.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Balance> GetBalanceAsync();
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>Current balance.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Balance> GetBalanceAsync(System.Threading.CancellationToken cancellationToken);
+
+        /// <returns>List of issued licenses.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<License>> ListLicensesAsync();
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>List of issued licenses.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<License>> ListLicensesAsync(System.Threading.CancellationToken cancellationToken);
+
+        /// <param name="args">Amount of money to spend for a license. Empty array for get free license. Maximum 10 active licenses</param>
+        /// <returns>Issued license.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<License> IssueLicenseAsync(System.Collections.Generic.IEnumerable<int> args);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <param name="args">Amount of money to spend for a license. Empty array for get free license. Maximum 10 active licenses</param>
+        /// <returns>Issued license.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<License> IssueLicenseAsync(System.Collections.Generic.IEnumerable<int> args, System.Threading.CancellationToken cancellationToken);
+
+        /// <param name="args">Area to be explored.</param>
+        /// <returns>Report about found treasures.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Report> ExploreAreaAsync(Area args);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <param name="args">Area to be explored.</param>
+        /// <returns>Report about found treasures.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Report> ExploreAreaAsync(Area args, System.Threading.CancellationToken cancellationToken);
+
+        /// <param name="args">License, place and depth to dig.</param>
+        /// <returns>List of treasures found.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<string>> DigAsync(Dig args);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <param name="args">License, place and depth to dig.</param>
+        /// <returns>List of treasures found.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<string>> DigAsync(Dig args, System.Threading.CancellationToken cancellationToken);
+
+        /// <param name="args">Treasure for exchange.</param>
+        /// <returns>Payment for treasure.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<int>> CashAsync(string args);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <param name="args">Treasure for exchange.</param>
+        /// <returns>Payment for treasure.</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<System.Collections.Generic.ICollection<int>> CashAsync(string args, System.Threading.CancellationToken cancellationToken);
+    }
+}
--- Services/LicenseService.cs
+++ Services/LicenseService.cs
@@ -4,29 +4,113 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MailRuCupMiner.Clients;
 using Mainerspace;
 
 namespace MailRuCupMiner.Services
 {
-    public class LicenseService
+    public class MinerLicense
     {
-        private Client _client;
-        private List<License> _licenses;
-        public LicenseService(Client client)
+        private License _license;
+        private int _digAllowed;
+        private int _digUsed;
+        private bool _isBusy = false;
+        public MinerLicense(License license)
+        {
+            _license = license;
+            _isBusy = false;
+            _digAllowed = license.DigAllowed;
+            _digUsed = license.DigUsed;
+            Id = license.Id;
+        }
+
+        public int Id { get; private set; }
+
+        public bool IsBusy()
+        {
+            return _isBusy;
+        }
+
+        public void SetBusy()
+        {
+            _isBusy = true;
+        }
+
+        public void SetUnLock()
+        {
+            _isBusy = false;
+        }
+
+        public bool CanDig()
+        {
+            if (_digAllowed > 0)
+                return true;
+            return false;
+        }
+
+        public void RegisterDig()
+        {
+            _digAllowed--;
+            _digUsed++;
+        }
+    }
+
+    public interface ILicenseService
+    {
+        Task<ICollection<MinerLicense>> GetFreeLicensesSlow();
+        MinerLicense GetFreeLicence();
+    }
+
+    public class LicenseService : ILicenseService
+    {
+        private IClient _client;
+        private List<MinerLicense> _licenses;
+        private object _lock = new object();
+        public LicenseService(IClient client)
         {
             _client = client;
             _licenses = GetFreeLicensesSlow()?.Result?.ToList();
         }
 
-        public async Task<ICollection<License>> GetFreeLicensesSlow()
+        public async Task<ICollection<MinerLicense>> GetFreeLicensesSlow()
         {
             var licenses = await _client.ListLicensesAsync();
-            return licenses;
+
+            var minerLicenses = new List<MinerLicense>();
+            foreach (var license in licenses)
+            {
+                var minerLicense = new MinerLicense(license);
+                minerLicenses.Add(minerLicense);
+            }
+
+            return minerLicenses;
         }
 
-        public License GetFreeLicence()
+        public MinerLicense GetFreeLicence()
         {
-            throw new NotImplementedException();
+            var free = _licenses.FirstOrDefault(license => !license.IsBusy());
+            if (free==null)
+            {
+                return null;
+            }
+
+            free.SetBusy();
+            return free;
+            
+        }
+
+        public void ReturnLicenseBack(MinerLicense license)
+        {
+            for (int i = 0; i < _licenses.Count; i++)
+            {
+                if (_licenses[i].Id == license.Id)
+                {
+                    _licenses[i] = license;
+                    _licenses[i].SetUnLock();
+                    return;
+                    
+                }
+            }
         }
     }
 }
